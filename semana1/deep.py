79fccccd9815742623fa6eab85a0ab7d414ee6ba
Jaime <jsarsaguillen@gmail.com>
2026-07-20 18:29:15 +0200

    hecho

---
+++ semana1/deep.py
@@ -0,0 +1,8 @@
+respuesta = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+
+match respuesta:
+    case "42" | "forty-two" | "forty two":
+        print("Yes")
+    case _:
+        print("No")
+
